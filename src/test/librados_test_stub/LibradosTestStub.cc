a24018434c00ac45711ee18a83112ec885e50532
Jason Dillaman <dillaman@redhat.com>
2015-06-24 18:35:58 -0400

    librados_test_stub: add support for new aio_notify API
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/librados_test_stub/LibradosTestStub.cc
+++ src/test/librados_test_stub/LibradosTestStub.cc
@@ -328,6 +328,14 @@ int IoCtx::aio_flush_async(AioCompletion *c) {
   return 0;
 }
 
+int IoCtx::aio_notify(const std::string& oid, AioCompletion *c, bufferlist& bl,
+                      uint64_t timeout_ms, bufferlist *pbl) {
+  TestIoCtxImpl *ctx = reinterpret_cast<TestIoCtxImpl*>(io_ctx_impl);
+  TestObjectOperationImpl *op = new TestObjectOperationImpl();
+  op->ops.push_back(boost::bind(&TestIoCtxImpl::notify, _1, _2, bl, timeout_ms, pbl));
+  return ctx->aio_operate(oid, *op, c->pc, NULL, 0);
+}
+
 int IoCtx::aio_operate(const std::string& oid, AioCompletion *c,
                        ObjectReadOperation *op, bufferlist *pbl) {
   return aio_operate(oid, c, op, 0, pbl);
