7945bc924f348376954c236e05d79101bd65a11f
Tianshan Qu <tianshan@xsky.com>
2018-06-19 15:00:22 +0800

    fix async_rados
    Signed-off-by: Tianshan Qu <tianshan@xsky.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -3733,33 +3733,29 @@ void RGWRados::finalize()
 {
   cct->get_admin_socket()->unregister_commands(this);
 
+  if (async_rados) {
+    async_rados->stop();
+  }
   if (run_sync_thread) {
     Mutex::Locker l(meta_sync_thread_lock);
     meta_sync_processor_thread->stop();
+    delete meta_sync_processor_thread;
+    meta_sync_processor_thread = NULL;
 
     Mutex::Locker dl(data_sync_thread_lock);
     for (auto iter : data_sync_processor_threads) {
       RGWDataSyncProcessorThread *thread = iter.second;
       thread->stop();
-    }
-    if (sync_log_trimmer) {
-      sync_log_trimmer->stop();
-    }
-  }
-  if (async_rados) {
-    async_rados->stop();
-  }
-  if (run_sync_thread) {
-    delete meta_sync_processor_thread;
-    meta_sync_processor_thread = NULL;
-    Mutex::Locker dl(data_sync_thread_lock);
-    for (auto iter : data_sync_processor_threads) {
-      RGWDataSyncProcessorThread *thread = iter.second;
       delete thread;
     }
     data_sync_processor_threads.clear();
-    delete sync_log_trimmer;
-    sync_log_trimmer = nullptr;
+
+    if (sync_log_trimmer) {
+      sync_log_trimmer->stop();
+      delete sync_log_trimmer;
+      sync_log_trimmer = nullptr;
+    }
+
     bucket_trim = boost::none;
   }
   if (finisher) {
